6caa60f798d1c4278145c1d3c237071fc763a9da
Plextora <71889427+Plextora@users.noreply.github.com>
2023-10-17 18:16:35 -0400

    Add actual functionality

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+// Currently only works on The Hive
 script.name = "Kill Notif";
 script.description =
     "Plays a sound whenever you kill someone on supported servers";
@@ -5,3 +6,12 @@ script.version = "0.0.0";
 script.author = "Plextora";
 let mod = new Module("KillNotif", "Kill Notif", script.description, 0 /* KeyCode.None */);
 client.getModuleManager().registerModule(mod);
+client.on("receive-chat", (ev) => {
+    var _a, _b;
+    if (ev.isChat && mod.isEnabled() && ((_a = game.getLocalPlayer()) === null || _a === void 0 ? void 0 : _a.isValid())) {
+        let playerName = (_b = game.getLocalPlayer()) === null || _b === void 0 ? void 0 : _b.getName();
+        if (ev.message.includes(`${playerName} §ckilled`)) {
+            game.playSoundUI("random.orb", 1, 1); // ding sound
+        }
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+// Currently only works on The Hive
+
 script.name = "Kill Notif";
 script.description =
   "Plays a sound whenever you kill someone on supported servers";
-script.version = "0.0.0";
+script.version = "1.1.0";
 script.author = "Plextora";
 
 let mod: Module = new Module(
@@ -12,3 +14,12 @@ let mod: Module = new Module(
 );
 
 client.getModuleManager().registerModule(mod);
+
+client.on("receive-chat", (ev) => {
+  if (ev.isChat && mod.isEnabled() && game.getLocalPlayer()?.isValid()) {
+    let playerName: string = game.getLocalPlayer()?.getName()!;
+    if (ev.message.includes(`${playerName} §ckilled`)) {
+      game.playSoundUI("random.orb", 1, 1); // ding sound
+    }
+  }
+});
